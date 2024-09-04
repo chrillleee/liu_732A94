ab6284eac3484a7348395db20ce1006da081bc6c
chrillleee <christian.jonsson6@gmail.com>
2024-09-04 18:11:17 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # liu_732A94
+This is my code for 732A94 Advanced Programming in R ( ECTS)
---
+++ lab1.R
@@ -0,0 +1,24 @@
+library(markmyassignment)
+lab_path <-
+"https://raw.githubusercontent.com/STIMALiU/AdvRCourse/master/Labs/Tests/lab1.yml"
+set_assignment(lab_path)
+
+# Assignment set:
+# Lab1: Advanced R programming, computer lab 1
+# The assignment contain the following (16) tasks:
+# - my num vector
+# - filter my vector
+# - dot prod
+# - approx e
+# - my magic matrix
+# - calculate elements
+# - row to zero
+# - add elements to matrix
+# - my magic list
+# - change info
+# - add note
+# - sum numeric parts
+# - my data.frame
+# - sort head
+# - add median variable
+# - analyze columns
