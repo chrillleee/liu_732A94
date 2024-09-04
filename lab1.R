f25d8da5ca2e0282267a7716df0e13aa12b10f7e
chrillleee <christian.jonsson6@gmail.com>
2024-09-04 23:05:52 +0200

    Performed some steps from lab

--- lab1.R
+++ lab1.R
@@ -22,3 +22,72 @@ set_assignment(lab_path)
 # - sort head
 # - add median variable
 # - analyze columns
+
+my_num_vector <- function() {
+    float_v <- c(log10(11), cos(pi/5), exp(pi/3), (1173%%7)/19)
+    return(float_v)
+}
+
+filter_my_vector <- function(x, leq){
+  output <- c()
+
+  for(num in x){
+    output <- append(output, ifelse(leq <= num, NA, num))
+  }
+
+  return(output)
+}
+
+ dot_prod <- function(a, b){
+  return(a %*% b)
+ }
+
+ approx_e <- function(N){
+    #approx N=11 gives  2.7183
+    iteration <- 0
+    output <- 0
+    while(iteration<=N){
+        output = output + 1/factorial(iteration)
+        iteration <- iteration + 1
+    }   
+    return(output)
+ }
+
+  my_magic_matrix <- function(){
+    output <- c()
+    output <- rbind(output, c(4,9,2))
+    output <- rbind(output, c(3,5,7))
+    output <- rbind(output, c(8,1,6))
+    return(output)
+  }
+
+  calculate_elements <-function(A){
+    return(length(A))
+  }
+
+  row_to_zero <- function(A, i){
+    A[i, ] <- A[i, ] * 0
+    return(A)
+  }
+
+add_elements_to_matrix <- function(A, x, i, j){
+    A[i,j] <- A[i,j] + x
+    return(A)
+}
+
+my_magic_list <-function(){
+  magic_list <- list(info = "my own list",
+  my_num_vector(),
+  my_magic_matrix())
+return(magic_list)
+}
+
+change_info <-function(x, text){
+    x[1]<-text
+    return(x)
+}
+
+add_note <-function(x, note){
+    x["note"] <- note
+    return(x)
+}
