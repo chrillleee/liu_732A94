c925a45969e2e2a4278373188b5d439ad07f983c
chrillleee <christian.jonsson6@gmail.com>
2024-09-09 22:07:12 +0200

    Added comments

--- lab1.R
+++ lab1.R
@@ -25,11 +25,13 @@ liuid <- "chjon338"
 # - add median variable
 # - analyze columns
 
+# comment: only syntax
 my_num_vector <- function() {
     float_v <- c(log10(11), cos(pi/5), exp(pi/3), (1173%%7)/19)
     return(float_v)
 }
 
+# comment: loop through the values in the vector and append the output vector and evaluate the number using the ternary operator  
 filter_my_vector <- function(x, leq){
   output <- c()
 
@@ -40,12 +42,14 @@ filter_my_vector <- function(x, leq){
   return(output)
 }
 
+# Here I had an issue with the commented out function, i checked the values from the unit test and my function outputted the same. You should check your tolerance on the unittest.
  dot_prod <- function(a, b){
   #return(a %*% b)
   return(sum(a * b))
 
  }
 
+# could have used a for loop but think the while makes it cleaner to state the purpose of the N
  approx_e <- function(N){
     #approx N=11 gives  2.7183
     iteration <- 0
@@ -57,6 +61,7 @@ filter_my_vector <- function(x, leq){
     return(output)
  }
 
+  # used rbind to make it clearer when reading the code to "see" the matrix
   my_magic_matrix <- function(){
     output <- c()
     output <- rbind(output, c(4,9,2))
@@ -65,20 +70,24 @@ filter_my_vector <- function(x, leq){
     return(output)
   }
 
+# used length
   calculate_elements <-function(A){
     return(length(A))
   }
 
+  # use matrix indexing to obtain the row vector of correct size, and insert on the right location 
   row_to_zero <- function(A, i){
     A[i, ] <- A[i, ] * 0
     return(A)
   }
 
+# use the provided indices to insert the value
 add_elements_to_matrix <- function(A, x, i, j){
     A[i,j] <- A[i,j] + x
     return(A)
 }
 
+# use the list function and the previously declared functions to create my list
 my_magic_list <-function(){
   magic_list <- list(info = "my own list",
   my_num_vector(),
@@ -86,16 +95,20 @@ my_magic_list <-function(){
 return(magic_list)
 }
 
+# since i know x contains the element "info" i can obtain the index by name and change it
 change_info <-function(x, text){
     x[["info"]]<- text
     return(x)
 }
 
+# add a new element by name indexing
 add_note <-function(x, note){
     x["note"] <- note
     return(x)
 }
 
+# Check if the element is numeric. Then assumes it is a numeric vector and sums it. This is a silly approach since i could have a list in a list and would result in missed values.
+# Could use unlist() and the do some evaluations but after some discussions with the lab assistant this solution was deemed sufficient.
 sum_numeric_parts <- function(x){
   output <- 0
   for(num in x){
@@ -106,6 +119,7 @@ sum_numeric_parts <- function(x){
   return(output)
 }
 
+# syntax
 my_data.frame <- function(){
   data.frame(
   id = c(1,2,3),
@@ -115,11 +129,13 @@ my_data.frame <- function(){
 )
 } 
 
+# breaked up the problem into 2, sort the column with the matching name in decending order and obtaining the indices. Then use the first n elements in the index vector to create a sorted matrix.
 sort_head <-function(df, var.name, n){
   indexes <- order(df[[var.name]],decreasing = TRUE)
   return(df[indexes[1:n],])
 }
 
+# 1) obtain median, 2) loop through the values and evaluate the conditions, 3) append the string into the string vector, 4) add the vector to the data frame
 add_median_variable<-function(df, j){
   median_val<- median(df[,j])
   output_str <- c()
@@ -138,6 +154,8 @@ add_median_variable<-function(df, j){
   return(df)
 }
 
+# 1) extract the right columns from the matrix, 2) obtain the names from the columns, 3) insert the data in the list 4) name the lists elements
+# Created a helper function to make it easier and cleaner 
 analyze_columns <-function(df, j){ 
   matrix_eval <- df[,j]
   theNames <- colnames(matrix_eval)
